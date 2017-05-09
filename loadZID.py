461deeb464dc6e5a03b2ba79f391d9be3d8e25db
kanderson102 <kvanderson102@gmail.com>
2017-05-09 15:58:14 -0500

    Refactored loadZID.py

--- loadZID.py
+++ loadZID.py
@@ -1,17 +1,18 @@
 import urllib.request
 import bs4 as bs
 import re
+from pyZillowProcessing import land_details
 
-def zpid_to_address_zip(zpid):
+def zpid_to_dict(zpid):
     sauce = urllib.request.urlopen("https://www.zillow.com/"
-                                   "homedetails/" + str(zpid) + "_zpid").read()
+                               "homedetails/" + str(zpid) + "_zpid").read()
     soup = bs.BeautifulSoup(sauce, 'lxml')
     tag = soup.title.string
-    address = re.match(r'(.+),\s(\w+),',tag)
+    address = re.search(r'(.+)\s\d\d\d\d\d',tag)
     zip = re.search(r'\s(\d\d\d\d\d)\s',tag)
 
     home_dict = {}
-    home_dict['address'] = address.group(1)
+    home_dict['address'] = address.group(0)
     home_dict['zip'] = zip.group(1)
 
     # prices = soup.find_all('span')
@@ -20,42 +21,59 @@ def zpid_to_address_zip(zpid):
     #         print(price.string)
 
     return home_dict
+
 def sqft_to_acres(sqft_lot):
-    return sqft_lot/(0.00002296)
+    return float(sqft_lot)*(0.00002296)
 
 def construct_zillow_URL(type,zip,min,max,):
     type = type
     zip = str(zip)
     min = str(min)
     max = str(max)
-
     sauce = urllib.request.urlopen("https://www.zillow.com/homes/for_sale/"
                                     ""+zip+"/"+type+"/"+min+"-"+max+"_price/").read()
 
     return sauce
 
-soup = bs.BeautifulSoup(construct_zillow_URL('land_type',28778,0,200000),'lxml')
-match = re.search('zpid:.+,pg',str(soup))   #string of zp-ID's
-zlist = re.findall('\d+',match.group())     #list of zp-ID's
-print('Compiled list of ZPIDs')
+def get_zip():
+    zip = True
+    while zip:
+        try:
+            zip = int(input(r"Search by zip code:"))
+        except ValueError:
+            print("Error: Must enter a five digit integer.")
+            continue
 
+        if len(str(zip)) != 5:
+            print("Must enter a five digit integer.")
+            continue
+        else:
+            return str(zip)
 
-from pyZillowProcessing import land_details
+def compile_list(zlist):
+    home_list = []
+    print("Compiling details...")
+    for id in zlist[:4]:
+        home = zpid_to_dict(id)
+        home_list.append(home)
 
-home_list = []
-for id in zlist[:4]:
-    home = zpid_to_address_zip(id)
-    home_list.append(home)
-    print('Added ' + id + ' to dict.')
-
-for home in home_list:
-    try:
+    for home in home_list:
         result = land_details(home['address'],home['zip'])
         home['zpid'] = result.zillow_id
         home['Type'] = result.home_type
-        home['Size'] = sqft_to_acres(result.property_size)
         home['Price'] = result.zestimate_amount
         home['Link'] = result.home_detail_link
+        home['Size'] = str(sqft_to_acres(result.property_size)) + ' acres'
         print(home)
-    except:
-        print('Error for {}'.format(home))
+
+zip = get_zip()
+soup = bs.BeautifulSoup(construct_zillow_URL('land_type',zip,0,200000),'lxml')
+match = re.search('zpid:.+,pg',str(soup))   #string of zp-ID's
+zlist = re.findall('\d+',match.group())     #list of zp-ID's
+if len(zlist)==0:
+    print("No properties available for zip code {}".format(zip))
+else:
+    print('Compiled list of ZPIDs')
+    compile_list(zlist)
+
+

9881efce4033aefe8765c221e8c65bf8dc9e7ddf
kanderson102 <kvanderson102@gmail.com>
2017-05-11 19:32:52 -0500

    added author to setup.py, removed API key from module

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(
 	name='zillowzip',
 	packages=find_packages(),
 	url='https://github.com/kanderson102/zillowzip',
+	author='Kyle Anderson',
 )
--- zillowzip/pyZillowProcessing.py
+++ zillowzip/pyZillowProcessing.py
@@ -1,7 +1,7 @@
 from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults
 
 def land_details(address, zip):
-    ZWSID = 'X1-ZWz19adsbelb0r_43hp8'
+    ZWSID = ''
     zillow_data = ZillowWrapper(ZWSID)
     deep_search_response = zillow_data.get_deep_search_results(address, zip)
     result = GetDeepSearchResults(deep_search_response)
